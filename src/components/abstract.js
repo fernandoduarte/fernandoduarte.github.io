050c9cf4694eeff8b1b5a46f53e3172093e22d2d
Helen Dempsey <helendempsey@helens-mbp.home>
2020-07-17 16:31:46 -0400

    Updates

--- src/components/abstract.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import abstractStyles from "./abstract.module.css"
-
-const Abstract = props => (
-    <div className={abstractStyles.abstract}>{props.children}</div>
-  )
-
-export default Abstract;
